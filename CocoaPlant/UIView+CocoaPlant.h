99632fda36c6d4e685701943f0c9917020f4af5c
Matt Di Pasquale <liveloveprosper@gmail.com>
2012-12-03 15:46:25 -0500

    Add UIView inlines: UIViewFadeOut & UIViewFadeIn.

--- CocoaPlant/UIView+CocoaPlant.h
+++ CocoaPlant/UIView+CocoaPlant.h
@@ -4,51 +4,42 @@
 // Apple makes these CG_EXTERN, but I'm not sure why. So, I made them CG_INLINE for ease.
 
 // Inset top of `rect' by `dy' -- i.e., increase origin.y by `dy', and decrease size.height by `dy'.
-CG_INLINE CGRect
-CGRectInsetTop(CGRect rect, CGFloat dy) {
+CG_INLINE CGRect CGRectInsetTop(CGRect rect, CGFloat dy) {
     rect.origin.y += dy; rect.size.height -= dy; return rect;
 }
 
 // Inset left of `rect' by `dx' -- i.e., increase origin.x by `dx', and decrease size.width by `dx'.
-CG_INLINE CGRect
-CGRectInsetLeft(CGRect rect, CGFloat dx) {
+CG_INLINE CGRect CGRectInsetLeft(CGRect rect, CGFloat dx) {
     rect.origin.x += dx; rect.size.width -= dx; return rect;
 }
 
 #pragma mark - UIView Inline Functions
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameOrigin(UIView *view, CGPoint origin) {
+UIKIT_STATIC_INLINE void UIViewSetFrameOrigin(UIView *view, CGPoint origin) {
     view.frame = CGRectMake(origin.x, origin.y, view.frame.size.width, view.frame.size.height);
 }
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameSize(UIView *view, CGSize size) {
+UIKIT_STATIC_INLINE void UIViewSetFrameSize(UIView *view, CGSize size) {
     view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, size.width, size.height);
 }
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameX(UIView *view, CGFloat x) {
+UIKIT_STATIC_INLINE void UIViewSetFrameX(UIView *view, CGFloat x) {
     view.frame = CGRectMake(x, view.frame.origin.y, view.frame.size.width, view.frame.size.height);
 }
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameY(UIView *view, CGFloat y) {
+UIKIT_STATIC_INLINE void UIViewSetFrameY(UIView *view, CGFloat y) {
     view.frame = CGRectMake(view.frame.origin.x, y, view.frame.size.width, view.frame.size.height);
 }
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameWidth(UIView *view, CGFloat width) {
+UIKIT_STATIC_INLINE void UIViewSetFrameWidth(UIView *view, CGFloat width) {
     view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, width, view.frame.size.height);
 }
 
-UIKIT_STATIC_INLINE void
-UIViewSetFrameHeight(UIView *view, CGFloat height) {
+UIKIT_STATIC_INLINE void UIViewSetFrameHeight(UIView *view, CGFloat height) {
     view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.size.width, height);
 }
 
-UIKIT_STATIC_INLINE UIViewAnimationOptions
-UIViewAnimationOptionsFromCurve(UIViewAnimationCurve curve) {
+UIKIT_STATIC_INLINE UIViewAnimationOptions UIViewAnimationOptionsFromCurve(UIViewAnimationCurve curve) {
     switch (curve) {
         case UIViewAnimationCurveEaseInOut:
             return UIViewAnimationOptionCurveEaseInOut;
@@ -61,4 +52,19 @@ UIViewAnimationOptionsFromCurve(UIViewAnimationCurve curve) {
         default:
             return curve;
     }
+}
+
+UIKIT_STATIC_INLINE void UIViewFadeOut(UIView *view) {
+    [UIView animateWithDuration:0.2 animations:^{
+        view.alpha = 0;
+    } completion:^(BOOL finished) {
+        view.hidden = YES;
+    }];
+}
+
+UIKIT_STATIC_INLINE void UIViewFadeIn(UIView *view) {
+    view.hidden = NO;
+    [UIView animateWithDuration:0.2 animations:^{
+        view.alpha = 1;
+    }];
 }
